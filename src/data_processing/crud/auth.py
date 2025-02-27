c62545e82df19e3f3df5c2ff550bbd88adf09016
Lyubomir <lyyubo@gmail.com>
2025-02-27 13:12:59 +0200

    Added function 'get_user_by_username'

--- src/data_processing/crud/auth.py
+++ src/data_processing/crud/auth.py
@@ -6,3 +6,17 @@ from sqlalchemy import and_, or_, func
 
 from src.data_processing.models.auth import User, Token, ApiKey
 from src.security.utils import get_password_hash, verify_password
+
+
+def get_user_by_username(db: Session, username: str) -> Optional[User]:
+    """
+    Returns a user by username
+
+    Args:
+        db: Database session
+        username: Username
+
+    Returns:
+        User object or None if not found
+    """
+    return db.query(User).filter(User.username == username).first()
