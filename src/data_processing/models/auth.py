e2e7c677f00de6236a31dbe331da3d98f19bd982
Lyubomir <lyyubo@gmail.com>
2025-02-27 12:36:55 +0200

    Created 'auth.py' for user Basic Security Implementation

---
+++ src/data_processing/models/auth.py
@@ -0,0 +1,6 @@
+from datetime import datetime, timedelta
+from sqlalchemy import Boolean, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+from src.data_processing.models.database import Base
+
