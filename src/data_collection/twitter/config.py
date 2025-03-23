d75b1cbab12c26532a6d46187c698c0e153bd728
Lyubomir <lyyubo@gmail.com>
2025-03-23 12:20:19 +0200

    Updating twitter/config.py

--- src/data_collection/twitter/config.py
+++ src/data_collection/twitter/config.py
@@ -77,6 +77,8 @@ class TwitterConfig(BaseSettings):
     )
 
     def __init__(self, **data):
+        data["is_test_mode"] = True
+
         # Get values from environment variables if they exist
         if TWITTER_API_KEY and TWITTER_API_KEY != "your_api_key":
             data["api_key"] = TWITTER_API_KEY
