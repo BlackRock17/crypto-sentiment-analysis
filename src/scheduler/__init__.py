2f3b78b48e16a08a184c30058c7b4afa5a56ec69
Lyubomir <lyyubo@gmail.com>
2025-04-10 17:18:17 +0300

    Deleted schemas and scheduler

--- src/scheduler/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Task scheduler module for Solana Sentiment Analysis.
-"""
-
-from src.scheduler.scheduler import setup_scheduler, shutdown_scheduler
-
-__all__ = ["setup_scheduler", "shutdown_scheduler"]
